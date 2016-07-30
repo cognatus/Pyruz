5a3930afe4fff392af443c975cd8efdf544c1482
AlexVanHell <alejandrodnl.cv@hotmail.com>
2016-07-30 02:56:35 -0500

    Diseno 3

---
+++ public/js/crearHuerto.js
@@ -0,0 +1,22 @@
+$.noConflict();
+
+jQuery(document).ready(function(){
+	
+	jQuery('#crearHuerto').click(function(){
+		jQuery('#hiddenAddHuerto').fadeIn();
+	});
+
+	jQuery('.orc_container .block').click(function(){
+		var dataHuerto = jQuery(this).parents('.orc_container').attr('data-num');
+		var dataLote = jQuery(this).attr('data-lote');
+		jQuery('#hiddenEditarLote').fadeIn();
+	});
+
+	jQuery('.close_popup').click(function(){
+		jQuery('.hidden_popup').fadeOut();
+	});
+
+	jQuery('.orc_container .block').css('height' , jQuery('.orc_container .block').width() * 0.7);
+
+
+});
