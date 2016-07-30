e0ae31729e8a1acf68a69acfa23a2d42e158513d
AlexVanHell <alejandrodnl.cv@hotmail.com>
2016-07-30 05:09:50 -0500

    Diseno 5

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@
 
 exports.index = function(req, res){
 	req.session.datos = [{_id: 'elchido@chido.com'}]
-	res.render('index', { title: 'Express' })
+	res.render('index', { title: 'Pyruz' })
 }
 
 exports.control = function(req, res){
-	res.render('index', { title: 'Express' })
+	res.render('control', { title: 'Pyruz' })
 }
